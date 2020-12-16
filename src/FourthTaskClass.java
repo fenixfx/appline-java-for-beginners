6529d4c2e9082e9256d3d9f6638d0d0bcfcf5324
ANagorskii <iplexus4@gmail.com>
2020-12-16 22:54:15 +0300

    Added fourth task

---
+++ src/FourthTaskClass.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FourthTaskClass {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int x, y, z;
+        x = scan.nextInt();
+        y = scan.nextInt();
+        z = scan.nextInt();
+        int res = (x + y + z) / 3;
+        String answer = res > 3 ? res + "\nПрограмма выполнена корректно" : "" + res;
+        System.out.println(answer);
+    }
+}
