010258e601ade64b06ff23bff9c1c4390073b747
ANagorskii <iplexus4@gmail.com>
2021-01-17 22:37:30 +0300

    Added seventh task

---
+++ src/Task7Class.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Task7Class {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int x = 1, y = 2, z = 3, inp;
+
+        System.out.println("Введите размер массива:");
+        int [] arr = new int[scan.nextInt()];
+
+        for(int i=0; i < arr.length; i++){
+            inp = scan.nextInt();
+            if (inp == x || inp == y || inp == z){
+                System.out.print("Данное значение имеется в константах");
+                break;
+            } else {
+                System.out.printf("В массив добавлен элемент: %s\n", inp);
+                arr[i] = inp;
+            }
+        }
+    }
+}
