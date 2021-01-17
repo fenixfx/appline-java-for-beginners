ab89644854bf23043019d6a4522573ad1fcafb60
ANagorskii <iplexus4@gmail.com>
2021-01-17 23:19:10 +0300

    Added eighth task

---
+++ src/Task8Class.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task8Class {
+
+    public static void main(String[] args){
+        int lim, sum = 0, i = 1;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите целое положительное число:");
+        lim = scan.nextInt();
+        sum += 1;
+        while (i + 2 <= lim){
+            i += 2;
+            sum += i;
+        }
+        System.out.print("Сумма всех нечетных чисел: " + sum);
+    }
+}
