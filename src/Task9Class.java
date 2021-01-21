c4c002fd62f79bdc373789c870b680d2bfae9148
ANagorskii <iplexus4@gmail.com>
2021-01-22 00:27:25 +0300

    Added ninth task

---
+++ src/Task9Class.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Task9Class {
+
+    public static void main(String[] args){
+        int listSize;
+        double numListAverage;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите размер массива:");
+        listSize = scan.nextInt();
+        if (listSize > 0) {
+            double[] numbersList = new double[listSize];
+            for (int i = 0; i < listSize; i++) {
+                numbersList[i] = scan.nextDouble();
+            }
+            numListAverage = calcNumListAverage(numbersList);
+            for (double el:numbersList) {
+                System.out.print(el * numListAverage + "\t");
+            }
+        } else {
+            System.out.println("Введено некорректное число");
+        }
+    }
+
+    public static double calcNumListAverage(double[] numbersList) {
+        double listAverage=0;
+        for (double el:numbersList) {
+            listAverage += el;
+        }
+        return listAverage / numbersList.length;
+    }
+}
